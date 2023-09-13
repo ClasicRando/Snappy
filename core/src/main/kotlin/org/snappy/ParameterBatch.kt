cde724930f70fa0a9af624a44a8aaaf68feb9b1e
Steven Thomson <steventhomson9@gmail.com>
2023-09-12 20:28:02 -0400

    feat: add extension method to convert ParameterBatch to list of SqlParameter

--- core/src/main/kotlin/org/snappy/ParameterBatch.kt
+++ core/src/main/kotlin/org/snappy/ParameterBatch.kt
@@ -1,5 +1,11 @@
 package org.snappy
 
+import org.snappy.extensions.toSqlParameterList
+
 interface ParameterBatch {
     fun toParameterBatch(): List<Any?>
+}
+
+fun ParameterBatch.toSqlParameterBatch(): List<SqlParameter> {
+    return toParameterBatch().toSqlParameterList()
 }
