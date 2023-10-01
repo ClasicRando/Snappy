66002aa9c79bacb5558a0b7fbdbfe913088118ce
Steven Thomson <steventhomson9@gmail.com>
2023-10-01 15:16:37 -0400

    doc: expand and clean up docs

--- core/src/main/kotlin/org/snappy/SnappyMapper.kt
+++ core/src/main/kotlin/org/snappy/SnappyMapper.kt
@@ -5,6 +5,14 @@ import org.snappy.cache.DecoderCache
 import org.snappy.cache.RowParserCache
 import java.io.File
 
+/**
+ * Main object storing cached data about an application using snappy. Although it's possible to
+ * allow the caches to be loaded only when needed, it's recommended to call [loadCache] during
+ * application startup to ensure the caches are populated before starting any operations that would
+ * otherwise be blocked by cache initialization. This would be important for any web application
+ * since without an initialized cache, the first request from a user to hit snappy method would be
+ * blocked until the cache is initialized.
+ */
 object SnappyMapper {
 
     private val config: SnappyConfig by lazy {
@@ -19,8 +27,10 @@ object SnappyMapper {
         config
     }
 
+    /** Cache of [org.snappy.rowparse.RowParser] classes available within the application */
     val rowParserCache = RowParserCache(config)
 
+    /** Cache of [org.snappy.decode.Decoder] classes available within the application */
     val decoderCache = DecoderCache(config)
 
     /**
@@ -32,6 +42,4 @@ object SnappyMapper {
         decoderCache.loadCache()
         rowParserCache.loadCache()
     }
-
-    init { loadCache() }
 }
