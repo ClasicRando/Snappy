0987f3a2a19e5972a9d413cc9187f0a3fab3deee
Steven Thomson <steventhomson9@gmail.com>
2023-10-21 10:08:24 -0400

    chore: mark logger methods as internal

--- core/src/main/kotlin/org/snappy/logging/Util.kt
+++ core/src/main/kotlin/org/snappy/logging/Util.kt
@@ -3,10 +3,11 @@ package org.snappy.logging
 import io.github.oshai.kotlinlogging.KLogger
 import io.github.oshai.kotlinlogging.KotlinLogging
 
-fun <R : Any> R.logger(): Lazy<KLogger> {
+@Suppress("UnusedReceiverParameter")
+internal fun <R : Any> R.logger(): Lazy<KLogger> {
     return lazy { KotlinLogging.logger {} }
 }
 
-fun logger(): Lazy<KLogger> {
+internal fun logger(): Lazy<KLogger> {
     return lazy { KotlinLogging.logger {} }
 }
